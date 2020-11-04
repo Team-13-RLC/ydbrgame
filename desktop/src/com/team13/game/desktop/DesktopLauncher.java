324a11d55b9554013516b47ac0413cc75bda0750
dk949 <david2000katz@gmail.com>
2020-11-04 19:53:39 +0000

    Adding the project

---
+++ desktop/src/com/team13/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.team13.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.team13.game.mainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new mainGame(), config);
+	}
+}
