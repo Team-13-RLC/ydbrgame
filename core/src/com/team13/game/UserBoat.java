58dea69c31f09be57a6fbad8fee7609027f9abca
saminmv <50016049+saminmv@users.noreply.github.com>
2020-11-11 19:26:23 +0000

    Update UserBoat.java

--- core/src/com/team13/game/UserBoat.java
+++ core/src/com/team13/game/UserBoat.java
@@ -8,16 +8,24 @@ public class UserBoat extends Boat {
     UserBoat(){
     }
     public void control(){
+        //when D is pressed, moving position to right
         if (Gdx.input.isKeyPressed(Input.Keys.D)){
             this.setPosX(this.getPosX() + this.getH_speed());
+            
+        //when A is pressed, moving position to left
         } else if (Gdx.input.isKeyPressed(Input.Keys.A)){
             this.setPosX(this.getPosX() - this.getH_speed());
+            
+        //when W is pressed, the boat is accelarating
         } else if (Gdx.input.isKeyPressed(Input.Keys.W)){
             accelerate(Direction.FORWARDS);
             this.setPosY(getPosY() + this.getV_speed());
+            
+        //when S is pressed, the boat is decelarating
         } else if (Gdx.input.isKeyPressed(Input.Keys.S)){
             accelerate(Direction.BACKWARDS);
             this.setPosY(getPosY() - this.getV_speed());
+            
         } else {
             this.update();
         }
@@ -25,6 +33,9 @@ public class UserBoat extends Boat {
     }
 
     public void update(){
+        /* when boat is moving but no keys are pressed,
+        the boat is slowly decelarating until it almost becomes stationary
+        */
         if (this.getV_speed() > 0){
             this.setV_speed(this.getV_speed() - this.getAcceleration());
         }
