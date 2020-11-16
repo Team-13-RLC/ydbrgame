32a0a10e07ab02bd4aa4b4614c6a271273a1c90f
dk949 <david2000katz@gmail.com>
2020-11-16 12:15:23 +0000

    There are lanes. They are being drawn.

--- core/src/com/team13/game/Canvas.java
+++ core/src/com/team13/game/Canvas.java
@@ -3,28 +3,44 @@ package com.team13.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Canvas {
-    private static Canvas instance = new Canvas();
-
     public OrthographicCamera camera;
+    private static Canvas instance = new Canvas();
     private Canvas(){}
 
 
+    private static final byte numLanes = 6;
+    private Lane[] lanes;
+
+
+    // Gets called once to init everything to do woth the canvas
     public void create(){
         camera = new OrthographicCamera(mainGame.Resolution.WIDTH, mainGame.Resolution.HEIGHT );
         camera.position.set(mainGame.Resolution.WIDTH/2f, mainGame.Resolution.HEIGHT/2f, 0f);
         camera.update();
-
-
-
-
     }
-
+    // Gets called every frame to update the canvas
     public void update(){
         Gdx.gl.glClearColor(0.4F, 0.4F, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        drawLanes();
+    }
 
+
+
+    public void makeLanes(){
+        lanes = new Lane[numLanes];
+        for (int i = 0; i < numLanes; i++){
+            lanes[i] = new Lane(i* (mainGame.Resolution.WIDTH/numLanes), (i+1)* (mainGame.Resolution.WIDTH/numLanes));
+        }
+    }
+
+    private void drawLanes(){
+        for (int i = 0; i < numLanes; i++){
+            lanes[i].draw(camera);
+        }
     }
 
 
@@ -33,4 +49,7 @@ public class Canvas {
         return instance;
     }
 
+    public Lane[] getLanes() {
+        return lanes;
+    }
 }
---
+++ core/src/com/team13/game/Lane.java
@@ -0,0 +1,44 @@
+package com.team13.game;
+
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class Lane {
+    // Consider changing these to short
+    private int lBorder;
+    private int rBorder;
+    // How wide the border of a lane is
+    private static final byte width = 5;
+    private ShapeRenderer shapeRenderer;
+    static private boolean projectionMatrixSet;
+
+
+
+    // Private constructor so borders never get initialised to 0
+    private Lane() {
+    }
+
+
+    public Lane(int lBorder, int rBorder) {
+        shapeRenderer = new ShapeRenderer();
+        this.lBorder = lBorder;
+        this.rBorder = rBorder;
+    }
+
+        public void draw(Camera camera){
+            shapeRenderer.setProjectionMatrix(camera.combined);
+
+            shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+            shapeRenderer.setColor(1, 0, 0, 1);
+            // Left border drawn (god this is awful)
+            shapeRenderer.rect( lBorder, 0, width, mainGame.Resolution.HEIGHT);
+            // Right border drawn
+            shapeRenderer.rect( rBorder - width, 0, width, mainGame.Resolution.HEIGHT);
+            shapeRenderer.end();
+
+    }
+
+
+}
