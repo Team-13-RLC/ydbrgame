07925445959bf784e6a64214b10e57a850a205d3
NamLDuong <namlinhduong@gmail.com>
2020-11-19 15:23:30 +0000

    Corrected the constructor for the Obstacle class. Created staticObstacle,dynamicObstacle, and Rock class. Decided that the image for the rock will be called rockTexture.png

---
+++ core/src/com/team13/game/obstacle/staticObstacle.java
@@ -0,0 +1,17 @@
+package com.team13.game.obstacle;
+
+import com.team13.game.stats.Position;
+
+public class staticObstacle extends Obstacle
+{
+
+    /**
+     * Inherits Obstacle class.
+     * @See Obstacle
+     */
+    public staticObstacle(Position position)
+    {
+        super(position);
+    }
+
+}
