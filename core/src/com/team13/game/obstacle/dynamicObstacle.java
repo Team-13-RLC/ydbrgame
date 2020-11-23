41e35a2b28a79a5704beba5bc3c0e2ab83f93fe7
Will-Dunlop <64005534+Will-Dunlop@users.noreply.github.com>
2020-11-23 17:23:49 +0000

    Added move function and made it use the velocity class.

--- core/src/com/team13/game/obstacle/dynamicObstacle.java
+++ core/src/com/team13/game/obstacle/dynamicObstacle.java
@@ -1,6 +1,7 @@
 package com.team13.game.obstacle;
 
 import com.team13.game.stats.Position;
+import com.team13.game.stats.Speed;
 
 public class dynamicObstacle extends Obstacle
 {
@@ -8,16 +9,27 @@ public class dynamicObstacle extends Obstacle
     /**
      * Directional speeds of an obstacle, in the X and Y directions.
      */
-    protected float velocityX;
-    protected float velocityY;
+    protected Speed velocity = new Speed(10,0);
+
 
 
     /**
      * Inherits Obstacle class.
-     * @See Obstacle
+     *
      */
     public dynamicObstacle(Position position)
     {
         super(position);
+
     }
+
+    /**
+     * Allows the obstacle to move with a set velocity.
+     */
+    public void ObstacleMove(){
+        Position updated = this.getObstaclePosition();
+        updated.setPosX(updated.getPosX() + velocity.getVelocityx());
+        this.setObstaclePosition(updated);
+    }
+
 }
